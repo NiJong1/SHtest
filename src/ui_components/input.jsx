2dbd21152cf6ed2ce8a955cccd274e245e4db1a1
NiJong1 <niekjongbloets@gmail.com>
2025-09-24 22:57:35 +0200

    Delete src/ui_components directory

--- src/ui_components/input.jsx
+++
@@ -1,8 +0,0 @@
-export function Input({ className = "", ...props }) {
-  return (
-    <input
-      className={`w-full border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-green-500 ${className}`}
-      {...props}
-    />
-  );
-}
